ce62ef40502d601005915869a9b6f8fe2658a045
Paul David Adam <drpauldavid.adam@gmail.com>
2026-09-20 17:07:35 +0100

    Add event listener for login button

--- scripts.js
+++ scripts.js
@@ -8,7 +8,9 @@ const videoPlayer = document.getElementById("videoPlayer");
 const placeholder = document.getElementById("placeholder");
 const downloadBtn = document.getElementById("downloadBtn");
 const loginBtn = document.getElementById("loginBtn");
-
+loginBtn.addEventListener("click", function() {
+  alert("LOGIN BUTTON IS WORKING");
+});
 // LOGIN
 window.login = function () {
   const username = prompt("Enter Login ID:");
